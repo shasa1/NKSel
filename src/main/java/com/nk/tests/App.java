a13dc79b7b03b576009b51c28a2bfb74c75efa0f
shasa1 <shasalman746@gmail.com>
2017-07-22 20:51:17 +0530

    Test CI

--- src/main/java/com/nk/tests/App.java
+++ src/main/java/com/nk/tests/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello samwise!" );
+        System.out.println("Checking the CI functionality");
     }
 }
